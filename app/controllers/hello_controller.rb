c22f4633779a947928213202c7e7e66f14f88a2d
OlekKovalevych <kovalevich.148@gmail.com>
2023-08-03 10:01:41 +0200

    Add plug-in to ChatGPT

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -2,6 +2,17 @@ class HelloController < ApplicationController
   def index
   end
 
+  def chat_gpt
+    cache = Rails.cache.read :conversation 
+    @conversation = cache || []
+    if params[:message_to_chat].present?
+      response_from_chat = ChatGptService.new(message: params[:message_to_chat]).call
+      @conversation.append(params[:message_to_chat])
+      @conversation.append(response_from_chat)
+    end
+    Rails.cache.write :conversation, @conversation, expires_in: 15.minutes
+  end
+
   def greeting
     @greeting = "Hello Rails"
   end
