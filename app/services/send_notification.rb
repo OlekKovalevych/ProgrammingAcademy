3b7d5f2a2fcb0785602614b18afb6c3aa7c8e95f
Marcelina Dykas <dykasmarcelina@gmail.com>
2023-08-17 14:38:49 +0200

    Env used in notification service

--- app/services/send_notification.rb
+++ app/services/send_notification.rb
@@ -7,7 +7,7 @@ class SendNotification
 
   def send
     # Put there the url of the channel you want to send the message to
-    uri = URI('https://discordapp.com/api/webhooks/1131490584595533844/EfHrDReupmhhD9W-JEmX6T4xxvgFH8YlJuuBGTD1nROyq3Ke47-HfNyAhkYAurW8BcFw')
+    uri = URI(ENV['DISCORD_URI'])
     res = Net::HTTP.post_form(uri, content: message )
     case res
     when Net::HTTPSuccess, Net::HTTPRedirection
